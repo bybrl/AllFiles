2b4d2d01720a5004d800dd0bd7bdcf1257cc2031
Thomas Green <113567858+NotMizzel@users.noreply.github.com>
2023-08-03 20:20:41 -0400

    Delete build directory

--- build/FileFunctions/CMakeFiles/FileFunctions.dir/filefunctions.cxx.obj.d
+++
@@ -1,2 +0,0 @@
-FileFunctions/CMakeFiles/filefunctions.dir/filefunctions.cxx.obj: \
- /C/Projects/AllFiles/src/FileFunctions/filefunctions.cxx
--- build/FileFunctions/CMakeFiles/getfile.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for getfile.
-# This may be replaced when dependencies are built.
